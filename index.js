aedb56ef574604b6749f26b3edf35ac11d5e77bb
m.baset@zoho.com <m.baset@zoho.com>
2025-05-28 20:28:05 +0300

    updates

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ const difficultySettings = {
 let currentDifficulty = "easy";
 
 function calculateCardDimensions(difficulty) {
-  const gap = 40;
+  const gap = 10;
   const { minCardWidth } = difficultySettings[difficulty];
   const cardsContainer = document.querySelector(".cards");
   const viewportWidth = cardsContainer.clientWidth;
